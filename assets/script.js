39bb2046742a0ea956ac3a607fdacd6df1f5dde0
ahmed hakeem <hakeemkoo@hotmail.com>
2021-04-02 00:23:40 -0400

    fix the backgrournd for the UV index

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
-//Declare a variable to store the searched city
-var city = "";
-// variable declaration
+
+
 var placesEl = $('#places');
 var searchCity = $("#search-city");
 var searchButton = $("#search-button");
@@ -10,49 +9,48 @@ var currentTemperature = $("#temperature");
 var currentHumidty = $("#humidity");
 var currentWSpeed = $("#wind-speed");
 var currentUvindex = $("#uv-index");
-var sCity = [];
+var myCities = [];
 var apiKey = "cd7b46258d74ce0db68ce9bc4d5cbf2e";
 
 
 
-searchButton.click(function () {
 
+
+searchButton.click(function () {
   //get name and its value
   var cities = $('input[id="search-city"]').val();
   //refactor url
   var weatherUrl = generateURL(cities);
-  currentWeather(weatherUrl)
+  currentWeather(weatherUrl);
+  fiveDayForecast(cities);
 
   //if empty
   if (!cities) {
-    alert("No cities chosen")
+    // alert("No cities chosen")
     return;
   }
-
-  //print
-  placesEl.append('<li>' + cities + '</li>');
-  $(placesEl).children().attr("class", "list-group-item")
-  $('.list-group').css('visibility', 'visible');
-  $('.btn-secondary').css('visibility', 'visible');
-  $('.list-group-item').click(function () {
-    var clickedCity = $(this).html();
-    currentWeather(weatherUrl)
-  })
-
+  printCities(cities)
+  currentWeather(weatherUrl)
   //clear input
   $('input[id="search-city"]').val('');
 
-  //save at local storage
-  localStorage.setItem('places', cities);
+  myCities.push(cities)
 
-});
+  //save at local storage
+  localStorage.setItem('places', JSON.stringify(myCities));
+
+})
+
+searchButton.click(function () {
+
+  $('#mainCity').show();
+  $('#future-weather').show();
+})
 
 function generateURL(cities) {
-  var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cities + '&appid=' + apiKey;
+  var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cities + '&units=metric&appid=' + apiKey;
   return weatherUrl;
 }
-
-
 
 function showWeather(event) {
   event.preventDefault();
@@ -61,7 +59,7 @@ function showWeather(event) {
     currentWeather(place);
   }
 };
-
+// get current weather
 function currentWeather(weatherUrl) {
   fetch(weatherUrl)
     .then(function (response) {
@@ -69,30 +67,36 @@ function currentWeather(weatherUrl) {
     })
 
     .then(function (data) {
-      var currentDate = moment().format('ll');
+      var currentDate = moment().format('L');
       var weatherIcon = data.weather[0].icon;
-      var iconUrl = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
-      console.log(iconUrl)
+      var iconUrl = 'https://openweathermap.org/img/wn/' + weatherIcon + '.png';
       //parse the response for name of city and concanatig the date and icon.
-      $('#current-city').text(data.name + " " + currentDate);
-      $("<img>").attr("src", iconUrl).prependTo("#current-weather")
-      var temp = (data.main.temp - 273.15);
+      $('#current-city').text((data.name.toUpperCase()) + " " + currentDate);
+      $("<img>").attr("src", iconUrl).appendTo("#current-city")
+      var temp = (data.main.temp);
       $('#temperature').text(" " + temp.toFixed(0) + ' °C');
       $('#humidity').text(" " + data.main.humidity + "%");
       $('#wind-speed').text(" " + data.wind.speed.toFixed(1) + ' MPH');
+
+      //get UV Index
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      var uvIndexUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+      fetch(uvIndexUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          $('#uv-index').text(" " + data.value)
+          if (data.value <= 2) {
+            $('#uv-index').css('background-color', 'yellow')
+          } else if (data.value <= 5) {
+            $('#uv-index').css('background-color', 'orange')
+          } else if (data.value <= 7) {
+            $('#uv-index').css('background-color', 'red')
+          } else {
+            $('#uv-index').css('background-color', 'violet')
+          }
+        })
     })
 }
-
-//get UV Index
- var lat = data.coord.lat;
- var lon = data.coord.lon;
- var uvIndexUrl = 'https://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
-  console.log(lat);
-  console.log(lon)
-  fetch(uvIndexUrl)
-   .then(function (response) {
-     return response.json();
-     })
-     .then(function (data) {
-        $('#uvIndex').text('UV Index: ' + data.value)
-      })
